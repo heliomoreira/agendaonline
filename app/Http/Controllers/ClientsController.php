73ed4078847ac6c5dd759eb467a88eb7dfdceb1d
Hélio Moreira <heliojsmoreira@gmail.com>
2025-06-13 02:07:40 +0100

    Implementation

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -5,56 +5,95 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CustomerRequest;
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ClientsController extends Controller
 {
     public function index()
     {
-        $clients = Client::all();
-        return view('modules.clients.index', [
-            'clients' => $clients
-        ]);
+        try {
+            $clients = Client::all();
+            return view('modules.clients.index', [
+                'clients' => $clients
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Erro ao listar clientes: ' . $e->getMessage());
+            return redirect()->back()->withErrors(__('Ocorreu um erro ao carregar a lista de clientes.'));
+        }
     }
 
     public function form()
     {
-        $client = new Client();
-        return view('modules.clients.form', [
-            'client' => $client
-        ]);
+        try {
+            $client = new Client();
+            return view('modules.clients.form', [
+                'client' => $client
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Erro ao carregar formulário de cliente: ' . $e->getMessage());
+            return redirect()->back()->withErrors(__('Ocorreu um erro ao carregar o formulário.'));
+        }
     }
 
     public function edit($id)
     {
-        $client = Client::find($id);
-        return view('modules.clients.form', [
-            'client' => $client
-        ]);
+        try {
+            $client = Client::findOrFail($id);
+            return view('modules.clients.form', [
+                'client' => $client
+            ]);
+        } catch (\Exception $e) {
+            Log::error("Erro ao editar cliente com ID {$id}: " . $e->getMessage());
+            return redirect()->route('clients.index')->withErrors(__('Cliente não encontrado ou ocorreu um erro.'));
+        }
     }
 
     public function store(CustomerRequest $request)
     {
-        $client = new Client();
-        $client->fill($request->all());
-        $client->save();
+        try {
+            $client = new Client();
+            $client->fill($request->all());
+            $client->save();
 
-        return redirect()->route('clients.edit', ['id' => $client->id])
-            ->with('success', __('modules.client_created'));
+            Log::info("Cliente criado com ID {$client->id}");
+
+            return redirect()->route('clients.edit', ['id' => $client->id])
+                ->with('success', __('modules.client_created'));
+        } catch (\Exception $e) {
+            Log::error('Erro ao criar cliente: ' . $e->getMessage());
+            return redirect()->back()->withInput()->withErrors(__('Ocorreu um erro ao criar o cliente.'));
+        }
     }
 
     public function update(CustomerRequest $request, $id)
     {
-        $client = Client::find($id);
-        $client->fill($request->all());
-        $client->id = $id;
-        $client->save();
+        try {
+            $client = Client::findOrFail($id);
+            $client->fill($request->all());
+            $client->save();
 
-        return redirect()->route('clients.edit', ['id' => $client->id])
-            ->with('success', __('modules.client_updated'));
+            Log::info("Cliente atualizado com ID {$client->id}");
+
+            return redirect()->route('clients.edit', ['id' => $client->id])
+                ->with('success', __('modules.client_updated'));
+        } catch (\Exception $e) {
+            Log::error("Erro ao atualizar cliente com ID {$id}: " . $e->getMessage());
+            return redirect()->back()->withInput()->withErrors(__('Ocorreu um erro ao atualizar o cliente.'));
+        }
     }
 
-    public function destroy()
+    public function destroy($id)
     {
+        try {
+            $client = Client::findOrFail($id);
+            $client->delete();
 
+            Log::info("Cliente removido com ID {$id}");
+
+            return redirect()->route('clients.index')->with('success', __('modules.client_deleted'));
+        } catch (\Exception $e) {
+            Log::error("Erro ao eliminar cliente com ID {$id}: " . $e->getMessage());
+            return redirect()->back()->withErrors(__('Ocorreu um erro ao eliminar o cliente.'));
+        }
     }
 }
