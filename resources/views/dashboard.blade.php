9dbfba56ed85d8c7d87d8f153708cc42bc69d509
Hélio Moreira <heliojsmoreira@gmail.com>
2025-06-13 01:04:52 +0100

    Implementation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,27 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+@extends('layouts.app')
+@section('content')
+    <div class="row g-6">
+        <!-- View sales -->
+        <div class="col-xl-4">
+            <div class="card">
+                <div class="d-flex align-items-end row">
+                    <div class="col-7">
+                        <div class="card-body text-nowrap">
+                            <h5 class="card-title mb-0">Congratulations John! 🎉</h5>
+                            <p class="mb-2">Best seller of the month</p>
+                            <h4 class="text-primary mb-1">$48.9k</h4>
+                            <a href="javascript:;" class="btn btn-primary">View Sales</a>
+                        </div>
+                    </div>
+                    <div class="col-5 text-center text-sm-left">
+                        <div class="card-body pb-0 px-0 px-md-4">
+                            <img src="{{asset('theme/assets/img/illustrations/card-advance-sale.png')}}"
+                                 height="140"
+                                 alt="view sales"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
