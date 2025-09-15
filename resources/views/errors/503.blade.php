46f732ff75b344ff2e943eaa7c89b65f57a0a106
Hélio Moreira <heliojsmoreira@gmail.com>
2025-09-15 18:02:32 +0100

    Global improvements

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -19,7 +19,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{global_global_asset('theme/assets/img/favicon/favicon.ico')}}" />
+    <link rel="icon" type="image/x-icon" href="{{global_asset('theme/assets/img/favicon/favicon.ico')}}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
