03de4fd7372923ad52f5b8bb4ba87bb324535960
Hélio Moreira <heliojsmoreira@gmail.com>
2025-09-16 16:24:54 +0100

    Global improvements

--- app/Services/VirtualminService.php
+++ app/Services/VirtualminService.php
@@ -12,7 +12,7 @@ class VirtualminService
     {
 
         try {
-            $url = config('virtualmin.url');
+            $url = "https://167.86.74.56:10000/virtual-server/remote.cgi";
 
             Log::info($url);
 
