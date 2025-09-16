dd0707717edcc2bdfae52367784d833ee803941e
Hélio Moreira <heliojsmoreira@gmail.com>
2025-09-16 17:04:02 +0100

    Global improvements

--- app/Services/VirtualminService.php
+++ app/Services/VirtualminService.php
@@ -25,6 +25,7 @@ class VirtualminService
                     'alias-with-dns' => config('tenancy.tenant_domain'),
                     'desc' => "Alias para " . config('tenancy.tenant_domain'),
                     'dns' => '',
+                    'web' =>''
                 ]
             ]);
 
