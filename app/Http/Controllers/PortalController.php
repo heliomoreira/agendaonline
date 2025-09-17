f8e51fa6b57004bfa0a94d7b06bf3cfa2207e39e
heliomoreira <heliojsmoreira@gmail.com>
2025-09-18 00:23:09 +0100

    Improvements

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -16,26 +16,31 @@ class PortalController extends Controller
 
     public function update(Request $request, $id, ImageService $imageService)
     {
-        $request->validate([
-            'logo' => ['nullable', 'image', 'max:2048'], // Ou 'profile_image' ou o que usares
-        ]);
+        try {
+            $request->validate([
+                'logo' => ['nullable', 'image', 'max:2048'], // Ou 'profile_image' ou o que usares
+            ]);
 
-        $updatePortal = Portal::find($id);
-        if ($updatePortal == null) {
-            $updatePortal = new Portal();
+            $updatePortal = Portal::find($id);
+            if ($updatePortal == null) {
+                $updatePortal = new Portal();
+            }
+
+            if ($updatePortal == null) {
+                return redirect()->route('dashboard');
+            }
+            $updatePortal->fill($request->except('logo'));
+
+            if ($request->hasFile('logo')) {
+                $path = $imageService->uploadImage($request->file('logo'), 'tenants/logos');
+                $updatePortal->logo = $path;
+            }
+
+            $updatePortal->save();
+            return redirect()->back()->with('success', 'Portal atualizado com sucesso.');
         }
-
-        if( $updatePortal == null){
-            return redirect()->route('dashboard');
+        catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Erro ao atualizar o portal: ' . $e->getMessage());
         }
-        $updatePortal->fill($request->except('logo'));
-
-        if ($request->hasFile('logo')) {
-            $path = $imageService->uploadImage($request->file('logo'), 'tenants/logos');
-            $updatePortal->logo = $path;
-        }
-
-        $updatePortal->save();
-        return redirect()->back()->with('success', 'Portal atualizado com sucesso.');
     }
 }
