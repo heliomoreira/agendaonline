c68760637e4c5dcb36ceadcc0a5f7fd3f38a3de7
Helio Moreira <heliojsmoreira@gmail.com>
2026-07-18 00:28:31 +0100

    Improvements

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Agenda;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportsController extends Controller
 {
-    public function index(){
-        $unpaid = Agenda::with(['service','client','paymentStatus'])->where('payment_status_id', 1)->get();
+    public function index()
+    {
+        $unpaid = Agenda::with(['service', 'client', 'paymentStatus'])->where('payment_status_id', 1)->orderBy('day')->where('day', '<', Carbon::today())->get();
         return view('admin.reports.index', compact('unpaid'));
     }
 }
