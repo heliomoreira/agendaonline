5b150ab99cf5c7b5e18c918b5ed45b674113a3db
Helio Moreira <heliojsmoreira@gmail.com>
2026-07-17 22:44:19 +0100

    Improvements

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,8 @@ use App\Http\Middleware\EnsurePortalActive;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -37,10 +37,7 @@ return Application::configure(basePath: dirname(__DIR__))
         });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->renderable(function (
-            TenantCouldNotBeIdentifiedOnDomainException $e,
-            Request $request
-        ): Response {
-            return response()->view('errors.tenant-not-found', [], 404);
+        $exceptions->render(function (TenantCouldNotBeIdentifiedOnDomainException $e, $request) {
+            return redirect()->away(config('app.url'));
         });
     })->create();
--- config/tenancy.php
+++ config/tenancy.php
@@ -23,6 +23,7 @@ return [
         '*.agendaonline.local',
         'agendaonline.pt',
         '*.agendaonline.pt',
+        'www.agendaonline.pt',
     ],
 
     /**
